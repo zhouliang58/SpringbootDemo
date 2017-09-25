e314954bcbe141cd717b1d529a3acedf30983011
liangduoduo666666 <798277403@qq.com>
2017-09-25 21:27:02 +0800

    spring boot学习

---
+++ src/main/java/com/zl/HelloController.java
@@ -0,0 +1,28 @@
+package com.zl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhouliang on 2017-09-22.
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @Value("${girl.age}")
+    private Integer age;
+
+    @Value("${content}")
+    private String content;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String say(){
+        return content+" "+girlProperties.getCupSize()+" "+age;
+    }
+}
